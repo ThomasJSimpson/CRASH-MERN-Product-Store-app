c77d59121f7a894b773892b2eb3c331022b90a19
Thomas Berenger <berenger.thomas@gmail.com>
2025-03-19 17:41:31 +0100

    ajout navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import CreatePage from "./pages/CreatePage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import Navbar from "./components/Navbar.jsx";
-
+import { useColorModeValue } from "@chakra-ui/react";
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
